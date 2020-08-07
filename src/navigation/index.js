c66e92ac82ad597665bc50d553ceab563e2024f1
bilal iftikhar <bilal.iftikhar@arbisoft.com>
2020-08-07 11:48:11 +0500

    :recycle: adding constants

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -4,14 +4,15 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {UserStackScreen} from '../screens/user/navigation';
 import {HomeStackScreen} from '../screens/home/navigation';
+import {PARENT_HOME_ROUTE, PARENT_USERS_ROUTE} from '../constants/navigation';
 
 const {Navigator, Screen} = createBottomTabNavigator();
 
 export const Navigation = () => (
   <NavigationContainer>
-    <Navigator initialRouteName="Home">
-      <Screen name="Home" component={HomeStackScreen} />
-      <Screen name="Users" component={UserStackScreen} />
+    <Navigator initialRouteName={PARENT_HOME_ROUTE}>
+      <Screen name={PARENT_HOME_ROUTE} component={HomeStackScreen} />
+      <Screen name={PARENT_USERS_ROUTE} component={UserStackScreen} />
     </Navigator>
   </NavigationContainer>
 );
